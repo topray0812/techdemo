6b29f2410f071446ce78cccfc795c62a551213a4
Sean Dai <Sean.Dai@city-holdings.com.au>
2019-09-03 09:34:09 +1000

    add runtime migration in todo project

--- TechDemo.Todo.API/Startup.cs
+++ TechDemo.Todo.API/Startup.cs
@@ -36,8 +36,10 @@ namespace TechDemo.Todo.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AuthDbContext dbContext)
         {
+            dbContext.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
