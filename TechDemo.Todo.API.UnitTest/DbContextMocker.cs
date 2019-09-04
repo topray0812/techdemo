89365825ac29eab8d09ebef6b4ab48041c7aa991
Sean Dai <Sean.Dai@city-holdings.com.au>
2019-09-04 15:04:38 +1000

    add unit test for todo project

---
+++ TechDemo.Todo.API.UnitTest/DbContextMocker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace TechDemo.Todo.API.UnitTest
+{
+    public static class DbContextMocker
+    {
+        public static TodoDbContext GetTodoDbContext()
+        {
+            IConfiguration _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                                                      .Build();
+
+            var conStr = _configuration.GetSection("ConnectionStrings").GetValue<string>("TechDemoConn");
+
+            var options = new DbContextOptionsBuilder<TodoDbContext>().UseNpgsql(conStr).Options;
+
+            var dbContext = new TodoDbContext(options);
+
+            dbContext.Database.Migrate();
+
+            return dbContext;
+        }
+    }
+}
---
+++ TechDemo.Todo.API.UnitTest/TodoControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using TechDemo.Todo.API.Services;
+using TechDemo.Todo.API.ViewModels;
+using Xunit;
+using System.Threading.Tasks;
+
+namespace TechDemo.Todo.API.UnitTest
+{
+    public class TodoControllerTests
+    {
+        [Fact]
+        public async Task TestGetAllTodoItemsAsync()
+        {
+            // Arrange
+            var dbContext = DbContextMocker.GetTodoDbContext();
+            var todoService = new TodoService(dbContext);
+
+            // Act
+            var items = todoService.GetAllTodoItems();
+
+            dbContext.Dispose();
+
+            // Assert
+            Assert.Single(items);
+        }
+
+        [Fact]
+        public async Task TestGetTodoItemAsync()
+        {
+            // Arrange
+            var dbContext = DbContextMocker.GetTodoDbContext();
+            var todoService = new TodoService(dbContext);
+
+            // Act
+            var item = todoService.GetTodoItemById(1);
+
+            dbContext.Dispose();
+
+            // Assert
+            Assert.Equal(1, item.Id);
+        }
+
+
+        [Fact]
+        public async Task TestUpdateTodoItemAsync()
+        {
+            // Arrange
+            var dbContext = DbContextMocker.GetTodoDbContext();
+            var todoService = new TodoService(dbContext);
+
+            var request = new TodoItemViewModel
+            {
+                Id = 1,
+                Title = "unit test title",
+                Description = "unit test description",
+                Latitude = -121.23126,
+                Longitude = 42.6235675
+            };
+
+            // Act
+            var result = todoService.UpdateTodoItem(request);
+
+            dbContext.Dispose();
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
